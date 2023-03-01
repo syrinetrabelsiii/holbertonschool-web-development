572ab287ce64ee43a1237bdf93f78ef62af0ae63
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-01 18:46:32 +0100

    closure and loops

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,8 @@
+function createClassRoom (numbersOfStudents)
+function studentSeat (seat) {
+    let students = [];
+    for ( let seat = 0; seat < numbersOfStudents; seat++)
+    return students;
+}
+
+let classRoom = createClassRoom(10);
